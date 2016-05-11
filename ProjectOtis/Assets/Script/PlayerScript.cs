63e1dd58f8e8eeb889cd1b905a353b279a1a9eac
Chris Kennedy <chriskiller93.3@gamail.com>
2016-05-11 04:14:06 -0400

    Player can run and it does rest when player leaves a place that slows them down.  It is also slows down when passing through things.

--- ProjectOtis/Assets/Script/PlayerScript.cs
+++ ProjectOtis/Assets/Script/PlayerScript.cs
@@ -5,6 +5,8 @@ public class PlayerScript : MonoBehaviour {
 
 	[SerializeField]
 	float Speed =.1f;
+	[SerializeField]
+	float MaxSpeed=.2f;
 	//private Transform PlayerTrans;
 
 	[SerializeField]
@@ -24,30 +26,66 @@ public class PlayerScript : MonoBehaviour {
 	{
 		if(Input.GetKey(KeyCode.W))
 		{
+			if (Input.GetKey (KeyCode.Space)) 
+			{
+				Vector3 Temp = this.transform.position;
+				Temp.y = Temp.y + MaxSpeed;
+				this.transform.position = Temp;
+			}
+			else
+			{
 			Vector3 Temp = this.transform.position;
 			Temp.y = Temp.y + Speed;
 			this.transform.position = Temp;
 			//Transform W = this.transform;
 			//W.position
 			//this.gameObject.transform.Translate(Vector3(PlayerTrans.position.x,PlayerTrans.position.y+Speed,PlayerTrans.position.x));
+			}
 		}
 		if(Input.GetKey(KeyCode.A))
 		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Vector3 Temp = this.transform.position;
+				Temp.x = Temp.x - MaxSpeed;
+				this.transform.position = Temp;
+			}
+			else
+			{
 			Vector3 Temp = this.transform.position;
 			Temp.x = Temp.x - Speed;
 			this.transform.position = Temp;
+			}
 		}
 		if (Input.GetKey (KeyCode.S))
 		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Vector3 Temp = this.transform.position;
+				Temp.y = Temp.y - MaxSpeed;
+				this.transform.position = Temp;
+			}
+			else
+			{
 			Vector3 Temp = this.transform.position;
 			Temp.y = Temp.y - Speed;
 			this.transform.position = Temp;
+			}
 		}
 		if (Input.GetKey (KeyCode.D)) 
 		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Vector3 Temp = this.transform.position;
+				Temp.x = Temp.x + MaxSpeed;
+				this.transform.position = Temp;
+			}
+			else
+			{
 			Vector3 Temp = this.transform.position;
 			Temp.x = Temp.x + Speed;
 			this.transform.position = Temp;
+			}
 		}
 	}
 
@@ -67,13 +105,16 @@ public class PlayerScript : MonoBehaviour {
 	public void SlowPlayer()
 	{
 		Speed = .065f;
+		MaxSpeed = .09f;
 	}
 	public void NormSpeedPlayer()
 	{
 		Speed = .1f;
+		MaxSpeed = .2f;
 	}
 	public void WaterSlowPlayer()
 	{
 		Speed = .05f;
+		MaxSpeed = .065f;
 	}
 }
