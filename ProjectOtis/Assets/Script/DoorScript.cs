1b52d33c857b4291de06e329ee1f8ac778dad103
Chris Kennedy <chriskiller93.3@gamail.com>
2016-05-10 20:28:27 -0400

    Player can pick up key and key obj does not display.  Door Only opens if the player as the right key.

---
+++ ProjectOtis/Assets/Script/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+	[SerializeField]
+	GameObject KeyID;
+	[SerializeField]
+	Sprite Open;
+	void OnTriggerEnter2D(Collider2D _other)
+	{
+		if (_other.gameObject.tag == "PlayerTag") 
+		{
+			PlayerScript play = _other.GetComponent<PlayerScript>();
+			GameObject ky = play.GetPlayerKey ();
+			if (ky == KeyID) 
+			{
+				SpriteRenderer spt = this.gameObject.GetComponent<SpriteRenderer> ();
+				spt.sprite = Open;
+			}
+		}
+	}
+}
---
+++ ProjectOtis/Assets/Script/PlayerScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	[SerializeField]
+	float Speed =.1f;
+	//private Transform PlayerTrans;
+
+	[SerializeField]
+	GameObject KeyID;
+	// Use this for initialization
+	void Start () {
+		//PlayerTrans = this.gameObject.transform;
+		KeyID = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			Vector3 Temp = this.transform.position;
+			Temp.y = Temp.y + Speed;
+			this.transform.position = Temp;
+			//Transform W = this.transform;
+			//W.position
+			//this.gameObject.transform.Translate(Vector3(PlayerTrans.position.x,PlayerTrans.position.y+Speed,PlayerTrans.position.x));
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			Vector3 Temp = this.transform.position;
+			Temp.x = Temp.x - Speed;
+			this.transform.position = Temp;
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			Vector3 Temp = this.transform.position;
+			Temp.y = Temp.y - Speed;
+			this.transform.position = Temp;
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			Vector3 Temp = this.transform.position;
+			Temp.x = Temp.x + Speed;
+			this.transform.position = Temp;
+		}
+	}
+
+	public void SetKey(GameObject Obj)
+	{
+		if (Obj.tag == "Key") 
+		{
+			KeyID = Obj;
+		}
+	}
+
+	public GameObject GetPlayerKey()
+	{
+		return KeyID;
+	}
+}
