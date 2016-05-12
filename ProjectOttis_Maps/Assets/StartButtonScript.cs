ebfec5d72c07c3be25685b26def3ddf05131cd73
jdcortes@fullsail.edu <jdcortes@fullsail.edu>
2016-05-12 02:18:38 -0400

    Menu Added

---
+++ ProjectOttis_Maps/Assets/StartButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class StartButtonScript : MonoBehaviour {
+
+    [SerializeField]
+    string SceneName;
+	public void StartGame()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
