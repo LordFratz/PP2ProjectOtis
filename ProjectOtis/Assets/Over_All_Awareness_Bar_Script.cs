da93f4b004aa0ec9e96d2ffd156dac50020b7661
Chris Kennedy <chriskiller93.3@gamail.com>
2016-05-12 09:38:29 -0400

    Player Stamina is in place and working.  HP is up as well as the awareness bar.

---
+++ ProjectOtis/Assets/Over_All_Awareness_Bar_Script.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Over_All_Awareness_Bar_Script : MonoBehaviour {
+
+	float value = 1;
+	public bool Is_Nothing_In
+	{
+		get 
+		{
+			if (value != 1)
+				return false;
+			else
+				return true;
+		}
+	}
+	public float Value
+	{
+		get
+		{
+			return value;
+		}
+		set
+		{
+			this.value = value;
+			transform.localScale = new Vector3(value, transform.localScale.y, transform.localScale.z);
+			if (value > 0.50)
+			{
+				Color C = new Color(1 - (this.value - 0.50f) * 2, 1, 0);
+				GetComponent<CanvasRenderer>().SetColor(C);
+			}
+			else
+			{
+				Color C = new Color(1, 0 + (this.value) * 2, 0);
+				GetComponent<CanvasRenderer>().SetColor(C);
+			}
+		}
+	}
+	void Start()
+	{
+		value = 0;
+		Color C = new Color(1, 0, 0);
+		GetComponent<CanvasRenderer>().SetColor(C);
+	}
+}
--- ProjectOtis/Assets/Script/PlayerScript.cs
+++ ProjectOtis/Assets/Script/PlayerScript.cs
@@ -7,6 +7,16 @@ public class PlayerScript : MonoBehaviour {
 	float Speed =.1f;
 	[SerializeField]
 	float MaxSpeed=.2f;
+	private float Stamina = 1;
+	private float HP = 1;
+	[SerializeField]
+	GameObject BarHP;
+	[SerializeField]
+	GameObject BarS;
+
+	private int Timer = 50;
+	private int StartTimerOnRecover;
+	private int StartTimer;
 	//private Transform PlayerTrans;
 
 	[SerializeField]
@@ -15,28 +25,46 @@ public class PlayerScript : MonoBehaviour {
 	void Start () {
 		//PlayerTrans = this.gameObject.transform;
 		KeyID = null;
+		StartTimer = Timer;
+		StartTimerOnRecover = Timer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	void FixedUpdate()
 	{
+		bool PlayerRan = false;
 		if(Input.GetKey(KeyCode.W))
 		{
-			if (Input.GetKey (KeyCode.Space)) 
+			if (Input.GetKey (KeyCode.Space)&& BarS.GetComponent<Bar_Script>().Is_Nothing_In == false) 
 			{
+				PlayerRan = true;
 				Vector3 Temp = this.transform.position;
 				Temp.y = Temp.y + MaxSpeed;
 				this.transform.position = Temp;
+				if (StartTimer > Timer)
+					StartTimer = Timer;
+				if (StartTimer == Timer)
+				{
+					Lower_Stamina ();
+					StartTimer = 0;
+				} else
+					StartTimer++;
+				//Lower_Stamina ();
 			}
 			else
 			{
 			Vector3 Temp = this.transform.position;
 			Temp.y = Temp.y + Speed;
 			this.transform.position = Temp;
+				StartTimer++;
+				//if (StartTimerOnRecover == Timer)
+				//	ReGain_Stamina ();
+				//else
+				//	StartTimerOnRecover++;
 			//Transform W = this.transform;
 			//W.position
 			//this.gameObject.transform.Translate(Vector3(PlayerTrans.position.x,PlayerTrans.position.y+Speed,PlayerTrans.position.x));
@@ -44,49 +72,119 @@ public class PlayerScript : MonoBehaviour {
 		}
 		if(Input.GetKey(KeyCode.A))
 		{
-			if(Input.GetKey(KeyCode.Space))
+			if(Input.GetKey(KeyCode.Space)&& BarS.GetComponent<Bar_Script>().Is_Nothing_In == false)
 			{
+				PlayerRan = true;
 				Vector3 Temp = this.transform.position;
 				Temp.x = Temp.x - MaxSpeed;
 				this.transform.position = Temp;
+				if (StartTimer > Timer)
+					StartTimer = Timer;
+				if (StartTimer == Timer)
+				{
+					Lower_Stamina ();
+					StartTimer = 0;
+				} else
+					StartTimer++;
+				//Lower_Stamina ();
 			}
 			else
 			{
 			Vector3 Temp = this.transform.position;
 			Temp.x = Temp.x - Speed;
 			this.transform.position = Temp;
+				StartTimer++;
+				//if (StartTimerOnRecover >= Timer)
+				//	ReGain_Stamina ();
+				//else
+				//	StartTimerOnRecover++;
 			}
 		}
 		if (Input.GetKey (KeyCode.S))
 		{
-			if(Input.GetKey(KeyCode.Space))
+			if(Input.GetKey(KeyCode.Space)&& BarS.GetComponent<Bar_Script>().Is_Nothing_In == false)
 			{
+				PlayerRan = true;
 				Vector3 Temp = this.transform.position;
 				Temp.y = Temp.y - MaxSpeed;
 				this.transform.position = Temp;
+				if (StartTimer > Timer)
+					StartTimer = Timer;
+				if (StartTimer == Timer)
+				{
+					Lower_Stamina ();
+					StartTimer = 0;
+				} else
+					StartTimer++;
+				//Lower_Stamina ();
 			}
 			else
 			{
 			Vector3 Temp = this.transform.position;
 			Temp.y = Temp.y - Speed;
 			this.transform.position = Temp;
+
+				StartTimer++;
+				//if (StartTimerOnRecover >= Timer)
+				//	ReGain_Stamina ();
+				//else
+				//	StartTimerOnRecover++;
 			}
 		}
 		if (Input.GetKey (KeyCode.D)) 
 		{
-			if(Input.GetKey(KeyCode.Space))
+			PlayerRan = true;
+			bool TempHolder=BarS.GetComponent<Bar_Script> ().Is_Nothing_In;
+			if(Input.GetKey(KeyCode.Space)&& TempHolder == false)
 			{
+				PlayerRan = true;
 				Vector3 Temp = this.transform.position;
 				Temp.x = Temp.x + MaxSpeed;
 				this.transform.position = Temp;
+				if (StartTimer > Timer)
+					StartTimer = Timer;
+				if (StartTimer == Timer)
+				{
+					Lower_Stamina ();
+					StartTimer = 0;
+				} else
+					StartTimer++;
+				//Lower_Stamina ();
 			}
 			else
 			{
 			Vector3 Temp = this.transform.position;
 			Temp.x = Temp.x + Speed;
 			this.transform.position = Temp;
+				//if (StartTimerOnRecover >= Timer)
+				//	ReGain_Stamina ();
+				//else
+				//	StartTimerOnRecover++;
+				StartTimer++;
 			}
 		}
+		if (StartTimerOnRecover > Timer)
+			StartTimerOnRecover = Timer;
+		if (StartTimer > Timer)
+			StartTimer = Timer;
+		if (PlayerRan == false) 
+		{
+			if (StartTimerOnRecover == Timer)
+				ReGain_Stamina ();
+			else
+				StartTimerOnRecover++;
+		}
+	}
+
+	private void Lower_Stamina()
+	{
+		BarS.GetComponent<Bar_Script> ().Value -= .1f;
+	}
+
+	private void ReGain_Stamina()
+	{
+		BarS.GetComponent<Bar_Script> ().Value += .1f;
+		StartTimerOnRecover = 0;
 	}
 
 	public void SetKey(GameObject Obj)
