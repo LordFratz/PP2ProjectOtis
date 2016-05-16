1aa25c7c5c8bba031c11571c545419c260dab9b3
Chris Kennedy <chriskiller93.3@gamail.com>
2016-05-15 21:33:23 -0400

    Ui is done

--- ProjectOttis_Maps/Assets/Assets_Chris/Script/Bar_Script.cs
+++ ProjectOttis_Maps/Assets/Assets_Chris/Script/Bar_Script.cs
@@ -10,6 +10,7 @@ public class Bar_Script : MonoBehaviour {
 	float BadSpot;
 	private bool test;
 	private float Max=1f;
+	private Color Yellow = new Color (1f, 1f, 0, 1f);
 	float Fill;
 	public bool Is_Nothing_In
 	{
@@ -56,27 +57,53 @@ public class Bar_Script : MonoBehaviour {
 		set
 		{
 			//show = Fill;
+			bool Up = false;
+			bool Down = false;
 			if (value >= 0&&value <1.1f) 
 			{
-				float tempm = 8.95f;
-   				Vector3 gift = transform.localPosition;
+				//float tempm = 8.95f;
+				float byHowMuch = 9.5f;
+  				Vector3 gift = transform.localPosition;
+
+				CanvasRenderer co =this.GetComponent<CanvasRenderer> ();
+				Color BadColor = co.GetColor();
 				if (value < this.Fill) 
 				{
-					if (transform.localPosition.x == 57.60001f) {
-						transform.localPosition = new Vector3 (41.1f, transform.localPosition.y, transform.localPosition.z);
+					Down = true;
+					float tempAdd = value * 10;
+					tempAdd = Mathf.FloorToInt (tempAdd);
+					tempAdd *= .1f;
+					float check = tempAdd - this.Fill;
+					if (check == .1f) {
+						transform.localPosition = new Vector3 ((Mathf.FloorToInt (transform.localPosition.x) + .4f) - byHowMuch, transform.localPosition.y, transform.localPosition.z);
 					} else {
-						transform.localPosition = new Vector3 (transform.localPosition.x - tempm, transform.localPosition.y, transform.localPosition.z);
+						transform.localPosition = new Vector3 ((Mathf.FloorToInt (transform.localPosition.x) + .4f) - Mathf.FloorToInt ((byHowMuch) * ((this.Fill - value) * 10)), transform.localPosition.y, transform.localPosition.y);
 					}
+					//if (transform.localPosition.x == 57.80001f) {
+					//	transform.localPosition = new Vector3 (41.1f, transform.localPosition.y, transform.localPosition.z);
+					//} else {
+					//	transform.localPosition = new Vector3 (transform.localPosition.x - tempm, transform.localPosition.y, transform.localPosition.z);
+					//}
 				}
 				//if(transform.localPosition.x ==57.60001f)
-				else 
-				{
-					if (transform.localPosition.x == 66.55001f) {
-						//67.4
-						transform.localPosition = new Vector3 (67.4f, transform.localPosition.y, transform.localPosition.z);
+				else {
+					Up = true;
+					float tempDec = value * 10;
+					tempDec = Mathf.FloorToInt (tempDec);
+					tempDec *= .1f;
+					float check = this.Fill - tempDec;
+					if (check == .1f) {
+						transform.localPosition = new Vector3 ((Mathf.FloorToInt (transform.localPosition.x) + .4f) - byHowMuch, transform.localPosition.y, transform.localPosition.z);
 					} else {
-						transform.localPosition = new Vector3 (transform.localPosition.x + tempm, transform.localPosition.y, transform.localPosition.z);
+						transform.localPosition = new Vector3 ((Mathf.FloorToInt (transform.localPosition.x) + .4f) - Mathf.FloorToInt ((byHowMuch) * ((this.Fill - value) * 10)), transform.localPosition.y, transform.localPosition.y);
 					}
+					//if (transform.localPosition.x == 57.80001f) 
+					//{
+					//	transform.localPosition = new Vector3 (58.2f, transform.localPosition.y, transform.localPosition.z);
+					//} else 
+					//{
+					//	transform.localPosition = new Vector3 (transform.localPosition.x + tempm, transform.localPosition.y, transform.localPosition.z);
+					//}
 				}
 				float temp = value * 10;
 				temp = Mathf.FloorToInt (temp);
@@ -84,6 +111,19 @@ public class Bar_Script : MonoBehaviour {
 				this.Fill = temp;
 				//transform.position =new Vector3 (transform.position.x-(value*1.5f), transform.position.y, transform.position.z);
 				transform.localScale = new Vector3 (value, transform.localScale.y, transform.localScale.z);
+				if (BadColor == Yellow) 
+				{
+					if (Up) 
+					{
+						//58.2f
+						transform.localPosition = new Vector3 (58.2f, transform.localPosition.y, transform.localPosition.z);
+
+					} else if (Down) 
+					{
+						//40.4f
+						transform.localPosition = new Vector3 (40.4f, transform.localPosition.y, transform.localPosition.z);
+					}
+				}
 				if (value > 0.50) 
 				{
 					Color C = new Color (1 - (this.Fill - 0.50f) * 2, 1, 0);
@@ -99,13 +139,17 @@ public class Bar_Script : MonoBehaviour {
 					//transform.localPosition = new Vector3 (posx, transform.localPosition.y, transform.localPosition.z);
 				}
 			}
-
+			if (Fill == Max) 
+			{
+				transform.localPosition = new Vector3 (94.4f, transform.localPosition.y, transform.localPosition.z);
+			}
 		}
 	}
 
 	void Start()
 	{
 		Fill = Max;
+		//Fill = .4f;
 		//shower = value;
 		Color C = new Color(0, Max, 0);
 		GetComponent<CanvasRenderer>().SetColor(C);
